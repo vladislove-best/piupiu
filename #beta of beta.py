7fd9ae7ce6caa9a0c73110f1f6024d91fd683755
vladislove-best <74818823+vladislove-best@users.noreply.github.com>
2021-01-30 14:12:28 +0300

    Add files via upload

---
+++ #beta of beta.py
@@ -0,0 +1,9 @@
+#beta of beta 
+import pygame
+pygame.init()
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('I kill you friend')
+black = (0,0,0)
+white = (255,255,255)
