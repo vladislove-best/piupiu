b3f5fa7f322e52b8962b5291d2f9762d5261fe2e
vladislove-best <74818823+vladislove-best@users.noreply.github.com>
2021-01-30 15:13:14 +0300

    Delete #beta of beta.py

--- #beta of beta.py
+++
@@ -1,9 +0,0 @@
-#beta of beta 
-import pygame
-pygame.init()
-display_width = 800
-display_height = 600
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('I kill you friend')
-black = (0,0,0)
-white = (255,255,255)
