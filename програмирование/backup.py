705385424f2a7866b6181adf765f7e23fa8195cc
vladislove-best <74818823+vladislove-best@users.noreply.github.com>
2021-05-01 15:30:57 +0300

    Add files via upload

--- програмирование/backup.py
+++ програмирование/backup.py
@@ -8,6 +8,7 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('XCOM 0.0.0.0.0.1')
 black = (0,0,0)
 white = (255,255,255)
+sword_invIMG = pygame.image.load('sword_in_bag.png')
 swordIMG = pygame.image.load('sword.png')
 staminIMG = pygame.image.load('stamin.png')
 healthIMG = pygame.image.load('health.png')
@@ -43,13 +44,17 @@ Sx = []
 Sy = []
 Bx = []
 By = []
-for i in range(3):
-    Sx.append(0)
-    Sy.append(0) 
+INV_p1 = []
+# for i in range(3):
+#     Sx.append(0)
+#     Sy.append(0) 
 for i in range(8):
     Bx.append(0)
     By.append(0)
 def sword (x,y):
+    gameDisplay.blit(swordIMG, (x,y))
+
+def sword_inv (x,y):
     gameDisplay.blit(swordIMG, (x,y))
 
 def health (x,y):
@@ -83,52 +88,64 @@ def move(allstep,x,y,a,b):
     oldy = y
     olda = a
     oldb = b
-    poop = False
-    right=False
-    left = False
-    up = False
-    down = False
-    while not poop:
-        for event in pygame.event.get():
-            ############################
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    left = True
-                    poop = True
-                elif event.key == pygame.K_RIGHT:
-                    right = True
-                    poop = True
-                elif event.key == pygame.K_UP:
-                    up = True
-                    poop = True
-                elif event.key == pygame.K_DOWN:
-                    down = True
-                    poop = True
-            ############################
-    else:
-        if right == True and a < 7:
-            a = a+1
-            x = X_list[a]  
-        if left == True and a > 0:
-            a = a-1
-            x = X_list[a]  
-        if up == True and b > 0:
-            b = b-1
-            y = Y_list[b]  
-        if down == True and b < 7:
-            b=b+1
-            y = Y_list[b] 
-        for i in range (len(By)):
-            if x==Bx[i] and y == By[i]:
-                x = oldx
-                y = oldy
-                a = olda
-                b = oldb
+    # poop = False
+    # right=False
+    # left = False
+    # up = False
+    # down = False
+    # while not poop:
+    #     for event in pygame.event.get():
+    #         ############################
+    #         if event.type == pygame.KEYDOWN:
+    #             if event.key == pygame.K_LEFT:
+    #                 left = True
+    #                 poop = True
+    #             elif event.key == pygame.K_RIGHT:
+    #                 right = True
+    #                 poop = True
+    #             elif event.key == pygame.K_UP:
+    #                 up = True
+    #                 poop = True
+    #             elif event.key == pygame.K_DOWN:
+    #                 down = True
+    #                 poop = True
+    #         ############################
+    # else:
+    if right == True and a < 7:
+        a = a+1
+        # print('right true')
+        x = X_list[a]  
+    if left == True and a > 0:
+        a = a-1
+        x = X_list[a]  
+    if up == True and b > 0:
+        b = b-1
+        y = Y_list[b]  
+    if down == True and b < 7:
+        b=b+1
+        y = Y_list[b] 
+    for i in range (len(By)):
+        if x==Bx[i] and y == By[i]:
+            x = oldx
+            y = oldy
+            a = olda
+            b = oldb
         # if allstep == 0:
         #     allstep = 3
         # print(allstep)
         dark(820,346) 
     return x,y,b,a
+def pick_up(a,b,player):
+    for i in range (len(Sx)):
+        if a == Sx[i] and b == Sy[i]:
+            del(Sx[i])
+            del(Sy[i])
+            if player == True:
+                sword_inv(774,565)
+            else:
+                sword_inv (774,74)
+
+            break
 def spawn(D1,D2):
     if D1 == 1:    
         a = random.randint(0,1)   
@@ -172,58 +189,76 @@ while not crashed:
         Bx[allbox] = X_list[random.randint(2,5)]
         By[allbox] = Y_list[random.randint(0,7)]
         allbox = allbox + 1
-    for event in pygame.event.get():
-        ############################
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                left = True
-            elif event.key == pygame.K_RIGHT:
-                right = True
-            elif event.key == pygame.K_UP:
-                up = True
-            elif event.key == pygame.K_DOWN:
-                down = True
-         ############################
-            if step ==True:
-                dark(752,491)
-                stamin(752,0)
-                stamin(752,110)
-                if allsteps >= 1:
-                    dark(752,601)
-                a_temp=a
-                while a_temp==a:  
-                    x,y,b,a = move(allstep,x,y,a,b)
-                if steps_passed%2==0:
-                    print(allstep) 
-                    for i in range(len(Sx)):
-                        Sx[i]= X_list[random.randint(2,5)]
-                        Sy[i]=Y_list[random.randint(0,7)]
-                # print(x,y)      
-                left = False
-                right = False
-                up = False
-                down = False
-                allsteps = allsteps + 1
-                if allsteps >= 2:
-                    step = False
-            else:
-                dark(752,0) 
-                stamin(752,491) 
-                stamin(752,601)
-                if allsteps <= 1:
-                    dark(752,110)
-                x2,y2,b2,a2 = move(allstep,x2,y2,a2,b2)
-                left = False
-                right = False
-                up = False
-                down = False 
-                allsteps = allsteps - 1
-                if allsteps <= 0:
-                    step = True
-                    steps_passed+=1       
+    step_done=False
+    while step_done==False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+                step_done=True
+                # print('crashed')
+            ############################
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    left = True
+                    step_done=True
+                elif event.key == pygame.K_RIGHT:
+                    right = True
+                    step_done=True
+                    # print('right')
+                elif event.key == pygame.K_UP:
+                    step_done=True
+                    up = True
+                elif event.key == pygame.K_DOWN:
+                    step_done=True
+                    down = True
+                elif event.key == pygame.K_SPACE:
+
+                ############################
+    if step ==True:
+        dark(752,491)
+        stamin(752,0)
+        stamin(752,110)
+        if allsteps >= 1:
+            dark(752,601)
+        # a_temp=a
+        # while a_temp==a:  
+        x,y,b,a = move(allstep,x,y,a,b)
+        # print(a, 'stepTrue')
+        # print(x,y)      
+        left = False
+        right = False
+        up = False
+        down = False
+        allsteps = allsteps + 1
+        pick_up(x,y,step)
+        if allsteps >= 2:
+            step = False
+        print(a,b, ' a b')
+        print(Sx, Sy, 'Sx Sy')
+    else:
+        dark(752,0) 
+        stamin(752,491) 
+        stamin(752,601)
+        if allsteps <= 1:
+            dark(752,110)
+        x2,y2,b2,a2 = move(allstep,x2,y2,a2,b2)
+        left = False
+        right = False
+        up = False
+        down = False 
+        allsteps = allsteps - 1
+        pick_up(x2,y2,step)
+        if allsteps <= 0:
+            step = True
+            steps_passed+=1  
+    if len(Sx)<2:
+        # print(allstep) 
+        Sx.append(X_list[random.randint(2,5)])
+        Sy.append(Y_list[random.randint(0,7)])
     gameDisplay.blit(b_g_Img,(0,0))
-    for i in range (2):
-        sword(Sx[i],Sy[i])
+    if len(Sx)>0:
+        for i in range(len(Sx)):
+            sword(Sx[i],Sy[i])
     # allstep = 0
     for i in range (8):
         box(Bx[i],By[i])
@@ -232,10 +267,8 @@ while not crashed:
     # if HDead == True:
         # spawn(1,0)    
     # if H2Dead == True:
-        # spawn(0,1)    
-    if event.type == pygame.QUIT:       
-        crashed = True     
+        # spawn(0,1)       
     pygame.display.update()
     clock.tick(60)
+   
 pygame.quit()
-quit()
